9a2218231983196cc94b5c40c104b34bb61fa4d6
luisxsssx <cd210902@gmail.com>
2024-03-03 09:33:07 -0700

    Cors configuration

--- src/main/java/com/backend/files/FilesApplication.java
+++ src/main/java/com/backend/files/FilesApplication.java
@@ -2,6 +2,9 @@ package com.backend.files;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FilesApplication {
@@ -10,4 +13,20 @@ public class FilesApplication {
 		SpringApplication.run(FilesApplication.class, args);
 	}
 
+	// Cors configuration
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:5173")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+
+						.allowedHeaders("*")
+						.allowCredentials(true) // Enable credentials
+						.maxAge(3600); // Controls preflight options cache time
+			}
+		};
+	}
 }

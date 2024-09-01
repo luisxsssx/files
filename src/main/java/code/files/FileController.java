10a7eeb970829f08e04a670049289d655e34348c
luisxsssx <cd210902@gmail.com>
2024-09-01 07:51:15 -0700

    Organizing code to fileService

--- src/main/java/code/files/FileController.java
+++ src/main/java/code/files/FileController.java
@@ -1,13 +1,9 @@
 package code.files;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.ResourceLoader;
 import code.files.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -44,16 +40,7 @@ public class FileController {
     @PostMapping("/folder/create")
     public ResponseEntity<String> createAnotherFolder(@RequestParam("folderName") String folderName,
                                                       @RequestParam(value = "parentFolder", required = false) String parentFolder) {
-        String folderPath = (parentFolder == null || parentFolder.isEmpty())
-                ? Paths.get(baseDir, folderName).toString()
-                : Paths.get(baseDir, parentFolder, folderName).toString();
-
-        File folder = new File(folderPath);
-        if (folder.mkdir()) {
-            return ResponseEntity.ok("Successfully created folder");
-        } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating folder");
-        }
+       return fileService.createFolder(folderName, parentFolder);
     }
 
     // Enter Folder
@@ -73,29 +60,7 @@ public class FileController {
     @PostMapping("/upload")
     public ResponseEntity<String> uploadFileToFolder(@RequestParam("file") MultipartFile file,
                                                      @RequestParam(value = "folderName", required = false) String folderName) {
-        String folderPath;
-        if(folderName == null || folderName.isEmpty()) {
-            folderPath = baseDir;
-        } else {
-            folderPath = Paths.get(baseDir, folderName).toString();
-            File folder = new File(folderPath);
-            if(!folder.exists()) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Folder does not exist");
-            }
-        }
-
-        String filePath = Paths.get(folderPath, file.getOriginalFilename()).toString();
-        try {
-            // Save the file to the file system
-            FileOutputStream fout = new FileOutputStream(filePath);
-            fout.write(file.getBytes());
-            fout.close();
-
-            return ResponseEntity.ok("File uploaded succesfully to:" + folderName);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error in uploading file: " + e.getMessage());
-        }
+        return fileService.uploadFile(file, folderName);
     }
 
     // Rename a file or folder
@@ -103,41 +68,13 @@ public class FileController {
     public ResponseEntity<String> rename(@RequestParam("currentName") String currentName,
                                          @RequestParam("newName") String newName,
                                          @RequestParam(value = "folderName", required = false) String folderName) {
-        String folderPath = (folderName == null || folderName.isEmpty()) ? baseDir : Paths.get(baseDir, folderName).toString();
-        File folder = new File(folderPath);
-        if (!folder.exists()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Folder does not exist");
-        }
-
-        File currentFileOrFolder = new File(Paths.get(folderPath, currentName).toString());
-        if (!currentFileOrFolder.exists()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("File or folder does not exist");
-        }
-
-        File newFileOrFolder = new File(Paths.get(folderPath, newName).toString());
-        if (currentFileOrFolder.renameTo(newFileOrFolder)) {
-            return ResponseEntity.ok("File or folder renamed successfully");
-        } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error renaming file or folder");
-        }
+        return fileService.rename(currentName, newName, folderName);
     }
 
     // Delete file/folder
     @DeleteMapping("/delete")
     public ResponseEntity<String> delete(@RequestParam("path") String path){
-        String targetPath = Paths.get(baseDir, path).toString();
-        File targetFileFolder = new File(targetPath);
-
-        if(!targetFileFolder.exists()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("File or folder not found");
-        }
-
-        if(fileService.deleted(targetFileFolder)) {
-            return ResponseEntity.ok("Succesfully deleted");
-        } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error deleting file or folder");
-        }
-
+       return fileService.delete(path);
     }
 
     // Move file to paper bin
@@ -160,23 +97,7 @@ public class FileController {
             @RequestParam(value = "path") String path,
             @RequestParam(value = "type", required = false) String type) throws FileNotFoundException {
 
-        String filePath = Paths.get(baseDir, path).toString();
-        File file = new File(filePath);
-
-        if (!file.exists()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-
-        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
-
-        HttpHeaders headers = new HttpHeaders();
-        String contentType = "application/octet-stream";
-        String headerValue = "attachment; filename=\"" + resource.getFilename() + "\"";
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, headerValue)
-                .body(resource);
+       return fileService.downloadFile(path, type);
     }
 
     // View files contents
@@ -194,6 +115,5 @@ public class FileController {
 
         return fileService.getFileResponse(filePath);
     }
-
 
 }
--- src/main/java/code/files/service/FileService.java
+++ src/main/java/code/files/service/FileService.java
@@ -2,14 +2,15 @@ package code.files.service;
 
 import code.files.model.fileModel;
 import code.files.model.folderModel;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.DecimalFormat;
@@ -23,6 +24,8 @@ import java.util.stream.Collectors;
 public class FileService {
 
     private static final DecimalFormat df = new DecimalFormat("#.##");
+
+    private String baseDir = "/home/luisxsssx/Documents/Code/documents/root/";
 
     // Helper method to delete files and directories recursively
     public boolean deleted(File fileOrFolder) {
@@ -54,7 +57,6 @@ public class FileService {
 
         return fileOrFolder.delete();
     }
-
 
     public List<Object> getFolderContent(String baseDir, String path, String type) {
         String fullPath = path != null ? Paths.get(baseDir, path).toString() : baseDir;
@@ -124,4 +126,97 @@ public class FileService {
         }
     }
 
+    public ResponseEntity<InputStreamResource> downloadFile(String path, String type) throws FileNotFoundException {
+        String filePath = Paths.get(baseDir, path).toString();
+        File file = new File(filePath);
+
+        if(!file.exists()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+        HttpHeaders headers = new HttpHeaders();
+        String contentType = "application/octet-stream";
+        String headerValue = "attachment; filename=\"" + resource.getFilename() + "\"";
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, headerValue)
+                .body(resource);
+    }
+
+    public ResponseEntity<String> delete(String path) {
+        String targetPath = Paths.get(baseDir, path).toString();
+        File targetFileFolder = new File(targetPath);
+
+        if(!targetFileFolder.exists()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("File or folder not found");
+        }
+
+        if(deleted(targetFileFolder)) {
+            return ResponseEntity.ok("Successfully deleted");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error deleting file or folder");
+        }
+    }
+
+    public ResponseEntity<String> rename(String currentName, String newName, String folderName) {
+        String folderPath = (folderName == null || folderName.isEmpty()) ? baseDir : Paths.get(baseDir, folderName).toString();
+        File folder = new File(folderPath);
+        if (!folder.exists()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Folder does not exist");
+        }
+
+        File currentFileOrFolder = new File(Paths.get(folderPath, currentName).toString());
+        if (!currentFileOrFolder.exists()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("File or folder does not exist");
+        }
+
+        File newFileOrFolder = new File(Paths.get(folderPath, newName).toString());
+        if (currentFileOrFolder.renameTo(newFileOrFolder)) {
+            return ResponseEntity.ok("File or folder renamed successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error renaming file or folder");
+        }
+    }
+
+    public ResponseEntity<String> uploadFile(MultipartFile file, String folderName) {
+        String folderPath;
+        if(folderName == null || folderName.isEmpty()) {
+            folderPath = baseDir;
+        } else {
+            folderPath = Paths.get(baseDir, folderName).toString();
+            File folder = new File(folderPath);
+            if(!folder.exists()) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Folder does not exist");
+            }
+        }
+
+        String filePath = Paths.get(folderPath, file.getOriginalFilename()).toString();
+        try {
+            // Save the file to the file system
+            FileOutputStream fout = new FileOutputStream(filePath);
+            fout.write(file.getBytes());
+            fout.close();
+
+            return ResponseEntity.ok("File uploaded succesfully to:" + folderName);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error in uploading file: " + e.getMessage());
+        }
+    }
+
+    public ResponseEntity<String> createFolder(String folderName, String parentFolder){
+        String folderPath = (parentFolder == null || parentFolder.isEmpty())
+                ? Paths.get(baseDir, folderName).toString()
+                : Paths.get(baseDir, parentFolder, folderName).toString();
+
+        File folder = new File(folderPath);
+        if (folder.mkdir()) {
+            return ResponseEntity.ok("Successfully created folder");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating folder");
+        }
+    }
 }

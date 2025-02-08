c697cbc07bf9aead27435234e9e6d40f434e5b58
luisxsssx <cd210902@gmail.com>
2025-02-07 17:08:20 -0700

    Changues to the CORS configuration

--- src/main/java/code/files/FilesApplication.java
+++ src/main/java/code/files/FilesApplication.java
@@ -13,6 +13,19 @@ public class FilesApplication {
 		SpringApplication.run(FilesApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
 
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true)
+						.maxAge(3600);
+			}
+		};
+	}
 
 }
